2b6c2c9d216a287f294ee367ba7ddce50dd49762
mahnafakmal <mahnafakmal@gmail.com>
2025-11-11 18:23:42 +0700

    kontak 1

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -5,7 +5,7 @@
   <h1>Kontak</h1><br>
   <img src="{{ $img }}" width="200px"> 
   <h2>perkenalkan nama saya Kokoh </h2> 
-  <h3>asal Pribumi</h3>
+  <h3>asal Pribumi</h3> <br>
   <p>@akmllhnf_</p>
   
 
